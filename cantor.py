917a8330e7de805cf0194d6447a6247776bcee18
BOCHENG LIN <152954589+TRIWOODBC@users.noreply.github.com>
2025-08-22 11:24:02 +1000

    Update cantor.py

--- cantor.py
+++ cantor.py
@@ -2,17 +2,17 @@ import torch
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-def cantor_mask(level: int, width: int, device: str = "cpu") -> torch.Tensor:
-    base = 3 ** level if level > 0 else 1
+def cantor_mask(level: int, width: int, device: str ) -> torch.Tensor:
+    base = 3 ** level if level > 0 else 1 
     x = torch.arange(width, device=device) * base // width
     mask = torch.ones(width, dtype=torch.bool, device=device)
     t = x.clone()
     for _ in range(level):
         mask &= (t.remainder(3) != 1)  # keep 0 and 2
-        t = torch.div(t, 3, rounding_mode='floor') # integer division
+        t = torch.div(t, 3, rounding_mode='floor') # integer division ternary digit
     return mask
 
-def cantor_ladder(n_levels=6, width=1200, bar_thickness=10, row_gap=8, device="cpu"):
+def cantor_ladder(device: str, n_levels=6, width=1200, bar_thickness=10, row_gap=8):
     height = (n_levels + 1) * (bar_thickness + row_gap) + row_gap
     img = torch.ones((height, width, 3), dtype=torch.float32, device=device)  # white bg
 
@@ -24,9 +24,9 @@ def cantor_ladder(n_levels=6, width=1200, bar_thickness=10, row_gap=8, device="c
     y = row_gap
     for level in range(n_levels + 1):
         mask1d = cantor_mask(level, width, device=device)  # (W,)
-        mask3d = mask1d.view(1, -1, 1).expand(bar_thickness, width, 3)
+        mask3d = mask1d.view(1, -1, 1).expand(bar_thickness, width, 3) # match the bar area
         color  = colors[level].view(1, 1, 3).expand(bar_thickness, width, 3)
-        band = img[y:y+bar_thickness, :, :]
+        band = img[y:y+bar_thickness, :, :] #Slice out the current bar region to be filled.
         img[y:y+bar_thickness, :, :] = torch.where(mask3d, color, band)
         y += bar_thickness + row_gap
     return img
@@ -39,4 +39,5 @@ plt.figure(figsize=(10, 3))
 plt.imshow(img.detach().cpu().numpy(), interpolation="nearest")
 plt.axis("off")
 plt.show()
+
 
